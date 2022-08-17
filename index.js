c068b676dbc29797be8e15d38c000c948738337f
PaulocastroDEV <pauloestudando159@gmail.com>
2022-08-17 10:45:34 -0300

    Geração do token

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app= express();
 const bodyParser= require("body-parser");
 const cors= require('cors')
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "skaoksoaksoaksokasokasoka";
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -139,8 +143,15 @@ app.post("/auth",(req,res)=>{
         if(user!=undefined){
 
             if(user.password==password){
-                res.status(200);
-                res.json({token: "Token Falso"});
+                jwt.sign({id:user.id,email: user.email},JWTSecret,{expiresIn:'48h'},(err,token)=>{
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token:token});
+                    }
+                })
             }else{
                 res.status(401);
                 res.json({err: "Credenciais inválidas"})
